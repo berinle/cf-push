4edd88de63d85e6c2f12d85509c78cb91089a9b2
Bayo Erinle <berinle@gmail.com>
2022-05-30 09:38:39 -0400

    debug script

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,4 +27,7 @@ fi
 #CF_USERNAME=${INPUT_USERNAME} CF_PASSWORD=${INPUT_PASSWORD} cf auth
 #cf target -o ${INPUT_ORG} -s ${INPUT_SPACE}
 #cf push -f ${INPUT_MANIFEST}
+echo `cf push ${INPUT_MANIFEST}`
+pwd
+ls -lrt
 cf push
